24c50dd3e94eefb5b0f631190d69e88525880ae3
do02reen24 <do02reen24@gmail.com>
2019-07-13 20:56:19 +0900

    Add testcode UpperCaseCounterTest

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -14,16 +14,16 @@ public class UpperCaseCounterTest {
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
-
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertFalse(numberOfUpperCaseCharactersInString!=0); //김도연
     }
 
     //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
-
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertFalse(numberOfUpperCaseCharactersInString==1); //김도연
     }
 
     //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
@@ -35,6 +35,7 @@ public class UpperCaseCounterTest {
 
         //assertTrue로 맞는 테스트 코드 작성
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(numberOfUpperCaseCharactersInString==8); //김도연
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
     }
 
@@ -45,19 +46,21 @@ public class UpperCaseCounterTest {
         int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
         //assertTrue로 맞는 테스트 코드 작성
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(result==3); //김도연
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
         System.out.println("result :: " + result);
     }
 
     //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class) //김도연
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
     }
 
     //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
     //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
-    @Test
+    @Ignore //김도연
+    @Test(timeout = 6000) //김도연
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
         System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
