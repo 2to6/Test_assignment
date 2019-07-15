41bf2e3498868ca3deaaa85d5e58f82a8d65d101
vihwan <vihwan@ajou.ac.kr>
2019-07-15 15:04:48 +0900

    Update Comment Use Personal Name

--- src/test/java/ChampionTest.java
+++ src/test/java/ChampionTest.java
@@ -41,21 +41,21 @@ public class ChampionTest {
     @Test
     public void givenCollectionWhenEmptyCorrect() {
         List<String> emptyList = new ArrayList<>();
-        assertThat(emptyList, empty());
+        assertThat(emptyList, empty()); //원동욱
     }
 
     //notNullValue 활용한 테스트
     @Test
     public void notNullCheck() {
         String lck = "LCK";
-        assertThat(lck, notNullValue());
+        assertThat(lck, notNullValue()); //원동욱
     }
 
     //nullValue 활용한 테스트
     @Test
     public void givenStringWhenNullIsCorrect() {
         String lck = null;
-        assertThat(lck, nullValue());
+        assertThat(lck, nullValue()); //원동욱
     }
 
 
@@ -72,17 +72,15 @@ public class ChampionTest {
         String sampleString2 = "Player point";
         String startString = "Player";
         String endString = "point";
-        assertThat(sampleString1, anyOf(startsWith("Player"), containsString("Focus"))); //성공
-        assertThat(sampleString1, anyOf(startsWith(startString), containsString(endString))); //성공
-        //  assertThat(sampleString1, allOf(startsWith(startString), containsString(endString))); //실패
-        assertThat(sampleString2, allOf(startsWith(startString), containsString(endString))); //성공
-        assertThat(sampleString2, is(endsWith(endString)));
+        assertThat(sampleString1, anyOf(startsWith("Player"), containsString("Focus"))); //원동욱
+        assertThat(sampleString1, anyOf(startsWith(startString), containsString(endString))); //원동욱
+        assertThat(sampleString2, allOf(startsWith(startString), containsString(endString))); //원동욱
+        assertThat(sampleString2, is(endsWith(endString))); //원동욱
     }
 
     //부동소수점 범위 closeTo 테스트
     @Test
     public void testForFloatingPoint() {
-//        assertThat(3.14, closeTo(3, 0.2));
         assertThat(3.14, closeTo(3,0.2)); //(실제값, (기준,오차범위))
     }
 
@@ -90,20 +88,15 @@ public class ChampionTest {
     @Test
     public void shouldNotErrorGetReference() {
         Champion whoareyou = championList.get(2);
-//        assertThat(championList.get(2), anything());
-        assertThat(championList.get(2), anything());
-        assertThat(championList.get(2), is(whoareyou));
+        assertThat(championList.get(2), anything()); //원동욱
+        assertThat(championList.get(2), is(whoareyou)); //원동욱
     }
 
     //객체 크기 검증 테스트 hasSize
     @Test
     public void shouldChampionCountFive() {
-//        assertTrue(championList.size() == 5);
-//        assertThat(championList.size(), is(5));
-//        assertThat(championList, hasSize(5));
-
-        assertTrue(championList.size() == 5);
-        assertThat(championList.size(), is(5));
+        assertTrue(championList.size() == 5); //원동욱
+        assertThat(championList.size(), is(5)); //원동욱
         assertThat(championList, hasSize(5)); //hasSize는 아얘 List자체를 던져주면 알아서 사이즈를 계산
 
     }
@@ -111,43 +104,34 @@ public class ChampionTest {
     //서폿 챔피언은 타릭이어야 한다라는 조건으로 테스트 코드 작성
     @Test
     public void shouldSupportChampionIsTaric() {
-        Champion supportChamp = new Champion("타릭", "바텀");
-//        assertThat("타릭", is(supportChamp.getName()));
-//        assertThat("타릭", is(equalTo(supportChamp.getName())));
-//        assertThat("타릭", equalTo(supportChamp.getName()));
-        assertThat("타릭", is(supportChamp.getName()));
-        assertThat("타릭", is(equalTo(supportChamp.getName())));
-        assertThat("타릭", equalTo(supportChamp.getName()));
+        Champion supportChamp = new Champion("타릭", "바텀"); //원동욱
+        assertThat("타릭", is(supportChamp.getName()));  //원동욱
+        assertThat("타릭", is(equalTo(supportChamp.getName()))); //원동욱
+        assertThat("타릭", equalTo(supportChamp.getName())); //원동욱
 
     }
 
     //hasProperty 활용하여 속성이 포함되어 있는지 테스트
     @Test
     public void shouldHasPropertyPosition() {
-//        assertThat(championList.get(0), hasProperty("position"));
-//        assertThat(championList.get(0), hasProperty("position", equalTo("탑")));
-        assertThat(championList.get(0), hasProperty("position", equalTo("탑")));
-        // position에 대한 프로퍼티가 있는지, 있다면 그
-        assertThat(championList.get(0), hasProperty("name"));
+        assertThat(championList.get(0), hasProperty("position", equalTo("탑"))); //원동욱
+        assertThat(championList.get(0), hasProperty("name")); //원동욱
     }
 
     //hasToString 활용 테스트
     @Test
     public void shouldHaveSomeChampName() {
-//        List<String> champListNames = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가느", "블라디미르");
-//        assertThat(champListNames.get(0), hasToString("루시안"));
-        List<String> FavoriteChampListNames = Arrays.asList("조이", "티모", "아이번", "징크스", "카밀");
-        assertThat(FavoriteChampListNames.get(0), hasToString("조이"));
-
+        List<String> FavoriteChampListNames = Arrays.asList("조이", "티모", "아이번", "징크스", "카밀"); //원동욱
+        assertThat(FavoriteChampListNames.get(0), hasToString("조이")); //원동욱
     }
 
     //property와 value가 같은지 테스트
     @Test
     public void shouldHaveSamePropertyAndValue() {
-        List<String> championNames1 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
-        List<String> championNames2 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미");
-//        assertThat(championNames1, samePropertyValuesAs(championNames2));
-        assertThat(championNames1,samePropertyValuesAs(championNames2)); // 두 객체의 프로퍼티값이 같은지
+        List<String> championNames1 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르"); //원동욱
+        List<String> championNames2 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미"); //원동욱
+        assertThat(championNames1,samePropertyValuesAs(championNames2)); //원동욱
+        // 두 객체의 프로퍼티값이 같은지
     }
 
 
@@ -155,34 +139,29 @@ public class ChampionTest {
     //탑 챔피언은 다리우스여야 한다라는 조건으로 테스트 코드 작성, stream 활용예
     @Test
     public void shouldTopChampionIsDarius() {
-//        Optional<Champion> filterdChampion = championList.stream()
-//                .filter(c -> c.getPosition().equals("탑"))
-//                .findFirst();
-//        String champName = filterdChampion.get().getName();
-//        assertTrue(champName.equals("다리우스"));
-//        assertThat("다리우스", is(champName));
-
         Optional<Champion> filterdChampion = championList.stream()
                 .filter(c -> c.getPosition().equals("탑"))
                 .findFirst(); //원하는 값만 필터링해서 값을 뽑아낼 수 있다.
-        System.out.println("result" + filterdChampion);
-        String champName = filterdChampion.get().getName();
-        assertTrue(champName.equals("다리우스"));
-        assertThat("다리우스", is(champName));
+        System.out.println("result" + filterdChampion); //원동욱
+        String champName = filterdChampion.get().getName(); //원동욱
+        assertTrue(champName.equals("다리우스")); //원동욱
+        assertThat("다리우스", is(champName)); //원동욱
     }
 
 
     @Test
     public void shouldSameInstance(){ //원동욱
         ArrayList testList = new ArrayList();
-        assertThat(testList, instanceOf(championList.getClass())); //동일한 인스턴스인지 타입을 비교한다.
-        assertThat("리신", sameInstance(championList.get(1).getName())); //Object가 완전히 동일한지를 비교한다.
+        assertThat(testList, instanceOf(championList.getClass())); //원동욱
+        //동일한 인스턴스인지 타입을 비교한다.
+        assertThat("리신", sameInstance(championList.get(1).getName())); //원동욱
+        //Object가 완전히 동일한지를 비교한다.
         //equals 비교가 아닌 주소비교로 작동
     }
 
     @Test
     public void shouldIgnoringWhiteSpace() { //원동욱
-        assertThat(championList.get(2).getPosition(), equalToIgnoringWhiteSpace("      미드       "));
+        assertThat(championList.get(2).getPosition(), equalToIgnoringWhiteSpace("      미드       ")); //원동욱
         //문자열의 앞뒤의 공백만을 무시한다.
     }
 }
